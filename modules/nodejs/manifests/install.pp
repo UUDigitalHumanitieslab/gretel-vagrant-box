6bfd58e8d8fdc07008d8a10bec45b7c5b0d40a23
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-02-26 11:54:12 +0100

    Upgrade node to 10.X

--- modules/nodejs/manifests/install.pp
+++ modules/nodejs/manifests/install.pp
@@ -1,6 +1,6 @@
 class nodejs::install{
   exec{ 'download':
-    command =>  'curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -',
+    command =>  'curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -',
     path =>     '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
   }
   exec{ 'sudo apt-get install -y nodejs':
